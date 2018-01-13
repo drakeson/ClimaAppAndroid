f5284eed64052dfd46e256228adccd2a3c6cfc97
drakesmith <ryansmith7119@gmail.com>
2018-01-14 00:36:58 +0300

    Initial commit

---
+++ app/src/main/java/smith/drake/kato/climaappandroid/ChangeCityController.java
@@ -0,0 +1,41 @@
+package smith.drake.kato.climaappandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class ChangeCityController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_city_layout);
+        final EditText query = findViewById(R.id.queryET);
+        ImageButton back = findViewById(R.id.backButton);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        query.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                String newCity = query.getText().toString();
+                Intent newIntent = new Intent(ChangeCityController.this, WeatherController.class);
+                newIntent.putExtra("City", newCity);
+                startActivity(newIntent);
+
+                return false;
+            }
+        });
+    }
+}
